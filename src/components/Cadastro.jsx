d7f91d0d53105b32d53b06d12ffff390091d2638
JuniorBarros <junior.dnbj55@gmail.com>
2026-02-21 17:56:42 +0000

    userState

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -1,21 +1,50 @@
 import styles from "./Cadastro.module.css"
 
+
+
+
 export default function Cadastro() {
+
+  const [name, setName] = useState([])
+  const [email, setEmail] = useState([])
+  const [age, setAge] = useState([])
+  const [users, setUsers] = useState([])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const newUser = {
+      name,
+      email,
+      age
+    }
+
+    setUsers([...users, newUser])
+
+    setName("")
+    setEmail("")
+    setAge("")  
+
+  }
 
 
   return (
-    <div className="app">
+    <div className={styles.app}>
       <h1>Cadastro de usuario</h1>
-      <form>
-<input placeholder="Nome" type="text"/>
-<input placeholder="Email" type="email"/>
-<input placeholder="Idade" type="number"/>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Nome" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder="Idade" type="number" value={age} onChange={(e) => setAge(e.target.value)} />
 
-<button type="submit">Cadastrar</button>
+        <button type="submit">Cadastrar</button>
       </form>
 
       <div className="user-list">
-        <p>Usuarios Cadastrados Aqui</p>
+        {users.map((user) => (
+          <div>
+            <p>{user.name}</p>
+          </div>
+        ))}
       </div>
 
     </div>
